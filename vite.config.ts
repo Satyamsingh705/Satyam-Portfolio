5d6df7520bd6e5c51c74776a530fd2972e750b3f
satyam <satyamsingh705071@gmail.com>
2025-04-25 13:34:05 +0530

    final chage

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Satyam-Portfolio/',  // Update this to match your actual repository name
+  base: process.env.NETLIFY === 'true' ? '/' : '/Satyam-Portfolio/',
   server: {
     watch: {
       usePolling: true,
